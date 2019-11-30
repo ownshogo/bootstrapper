1a3393e90143f03577b15b88ced87237f9e1dfb4
Shogo Osawa <57659371+ownshogo@users.noreply.github.com>
2019-11-30 22:27:49 +0900

    Create install script (setup.py).

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bootstrapper-SHOGO-OSAWA", # Replace with your own username
+    version="0.0.1",
+    author="Shogo Osawa",
+    author_email="57659371+ownshogo@users.noreply.github.com",
+    description="A Python library for bootstrap.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ownshogo/bootstrapper",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=['joblib>=0.14.0', 'numpy>=1.17.4']
+)
