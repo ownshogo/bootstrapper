05776427227ab5b2436776dc9af322b9ca683d82
Shogo Osawa <57659371+ownshogo@users.noreply.github.com>
2019-11-30 20:45:00 +0900

    Multiple samples handling.

--- bootstrapper/bootstrapper.py
+++ bootstrapper/bootstrapper.py
@@ -18,15 +18,15 @@ class Bootstrapper:
             raise ValueError('bootstrap_count must be positive integer. {} was given.'.format(bootstrap_count))
         self.bootstrap_count = bootstrap_count
 
-    def run(self, sample, function):
+    def run(self, function, *samples):
         """Run bootstrapping process using the given function.
 
         Parameters
         ----------
-        sample: numpy 1d array
-            The sample to be bootstrapped.
         function: callable
-            The function applied to bootstrap sampling. Must take numpy 1d-array as its first argument.
+            The function applied to bootstrap sampling. Must take numpy 1d-arrays as its vararg.
+        samples: numpy 1d arrays
+            The samples to be bootstrapped.
 
         Returns
         -------
@@ -34,7 +34,7 @@ class Bootstrapper:
             The function output applied to each bootstrap sampling.
         """
         def bootstrap():
-            bs = numpy.random.choice(sample, sample.size, replace=True)
-            return function(bs)
+            bs = [numpy.random.choice(s, s.size, replace=True) for s in samples]
+            return function(*bs)
 
         return joblib.Parallel(n_jobs=self.n_jobs)(joblib.delayed(bootstrap)() for i in range(self.bootstrap_count))
--- example.py
+++ example.py
@@ -3,9 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from bootstrapper import Bootstrapper
 
-sample = numpy.random.randn(1000000)
-b = Bootstrapper(bootstrap_count=1024)
-means = b.run(sample, lambda array: array.mean())
+sample = numpy.random.randn(1_000_000)
+b = Bootstrapper()
+means = b.run(lambda array: array.mean(), sample)
 
 sns.set()
 sns.distplot(means, kde=True)
